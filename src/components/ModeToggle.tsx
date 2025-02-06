38af2ff416efb757d1a02fad6f65345637f192a9
OHSOHYEON00 <ohsohyeon24@gmail.com>
2025-02-06 12:02:25 +0100

    Fix apply tailwind config file issue, Finish Nav

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,44 +1,41 @@
-import { useState, useEffect } from "react"
-import { Moon, Sun } from "lucide-react"
-
-// import { Button } from "@/components/ui/button"
+import { useState, useEffect } from "react";
+import { TiWeatherSunny } from "react-icons/ti";
+import { LuMoon } from "react-icons/lu";
 
 export function ModeToggle() {
   const [theme, setThemeState] = useState<"theme-light" | "dark" | "system">(
     "theme-light"
-  )
+  );
 
   const toggleTheme = () => {
-    const newTheme = theme === "dark" ? "theme-light" : "dark"
-    setThemeState(newTheme)
-    localStorage.setItem("theme", newTheme)
-  }
+    const newTheme = theme === "dark" ? "theme-light" : "dark";
+    setThemeState(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
 
   useEffect(() => {
-    const isDarkMode = document.documentElement.classList.contains("dark")
-    setThemeState(isDarkMode ? "dark" : "theme-light")
-  }, [])
+    const isDarkMode = document.documentElement.classList.contains("dark");
+    setThemeState(isDarkMode ? "dark" : "theme-light");
+  }, []);
 
   useEffect(() => {
     const isDark =
       theme === "dark" ||
-      window.matchMedia("(prefers-color-scheme: dark)").matches
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-    document.documentElement.classList[isDark ? "add" : "remove"]("dark")
-  }, [theme])
+    document.documentElement.classList[isDark ? "add" : "remove"]("dark");
+  }, [theme]);
 
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      className="rounded-full"
+    <button
+      className=" cursor-pointer hover:scale-90 duration-150 "
       onClick={toggleTheme}
     >
       {theme === "theme-light" ? (
-        <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <TiWeatherSunny className=" bg-main h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       ) : (
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <LuMoon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       )}
-    </Button>
-  )
+    </button>
+  );
 }
